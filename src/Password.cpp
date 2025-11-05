66c11a7e78bb3a910c17b64fda1695e6ddbb05db
XYYOVI <akinfeev_e2007@mail.ru>
2025-11-05 23:15:13 +0300

    Renamed first_project and add one more project
    renamed first_project and add one more project

---
+++ src/Password.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int password() {
+	system("chcp 1251 > nul");
+	cout << "******** ГЕНЕРАТОР ПАРОЛЕЙ ********" << endl;
+	cout << "Выберите тип пароля:" << endl << "1.Буквенный" << endl << "2.Цифровой" << endl << "3.Буквенно - цифровой" << endl;
+	return 1;
+}
+
+int main() {
+	password();
+	return 1;
+}
