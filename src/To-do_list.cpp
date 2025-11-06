6aefaca56e7a58066631d3ded8db229277025ec0
XYYOVI <akinfeev_e2007@mail.ru>
2025-11-06 21:28:44 +0300

    Add something to to_do list

--- src/To-do_list.cpp
+++ src/To-do_list.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
+	system("chcp 1251 > nul");
+	cout << "******** ПЛАНИРОВЩИК ДЕЛ ********" << endl << endl;
+	cout << "Выберите категорию:\n1.Работа\n2.Образование\n3.Спорт\n4.Финансы\n5.Дом\n6.Хобби\n7.Личное\n\nВаш выбор:\n";
 	return 0;
 }
